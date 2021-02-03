66719c5001b4ca11c8a20dc7d18b7d75b4695d06
Samir <samir.irshaid@gmail.com>
2021-02-03 16:38:04 +0100

    a

---
+++ circumference of  Rectangle & Circle.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n",
+    "w = int(input(\" Enter the width =\") )\n",
+    "c = l*w\n",
+    "print(\"The circumference of  rectangle = \",c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import math\n",
+    "\n",
+    "radius = float(input(\"Enter the radius of the circle : \"))\n",
+    "\n",
+    "circumference = 2 * math.pi * radius\n",
+    "\n",
+    "print(circumference)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
